df95321cb763c8cbd8dc32169a6c2983d018859d
James Wilkinson <jw@jameswilko.com>
2018-01-17 22:57:07 +0100

    Added helper defines to add new signatures easier Added code to store all wanted signatures and search for them Added proof of concept way of getting the SQSharedState which isn't accessible on the SQVM struct normally

--- TitanfallSDK/Signatures/SigScan.cpp
+++ TitanfallSDK/Signatures/SigScan.cpp
@@ -5,28 +5,19 @@
 #include <tchar.h>
 
 #include "SigScan.h"
+#include "../SquirrelTypes.h"
 #include "../Utils/Util.h"
 
 #include "MinHook.h"
 #if defined _M_X64
-#pragma comment(lib, "libMinHook-x64-v140-mt.lib")
+#pragma comment(lib, "libMinHook-x64-v140-mtd.lib")
 #elif defined _M_IX86
-#pragma comment(lib, "libMinHook-x86-v140-mt.lib")
+#pragma comment(lib, "libMinHook-x86-v140-mtd.lib")
 #endif
 
 using namespace Util;
 
-// Squirrel 2.2.3 stable
-typedef __int64 SQInteger;
-typedef SQInteger SQRESULT;
-typedef char SQChar;
-typedef struct SQVM* HSQUIRRELVM;
-
-typedef int (WINAPI *MESSAGEBOXW)(HWND, LPCWSTR, LPCWSTR, UINT);
-typedef SQRESULT( *sq_throwerror )(HSQUIRRELVM, const SQChar *);
-
-std::vector<SignatureF>* allSignatures = NULL;
-sq_throwerror fpSqThrowError = nullptr;
+std::vector<FSignature> * AllSignatures;
 
 MODULEINFO GetModuleInfo( std::string szModule )
 {
@@ -38,30 +29,112 @@ MODULEINFO GetModuleInfo( std::string szModule )
 	return modinfo;
 }
 
-SignatureSearch::SignatureSearch( const char* funcname, void* adress, const char* signature, const char* mask, int offset )
+SignatureSearch::SignatureSearch( const std::string ModuleName, const std::string FuncName, void * Address, void * HookAddress, const std::string Signature, const std::string Mask, int Offset )
 {
-	// lazy-init, container gets 'emptied' when initialized on compile.
-	if ( !allSignatures )
+	if ( AllSignatures == NULL )
 	{
-		allSignatures = new std::vector<SignatureF>();
+		AllSignatures = new std::vector<FSignature>();
 	}
 
-	SignatureF ins = { funcname, signature, mask, offset, adress };
-	allSignatures->push_back( ins );
+	FSignature NewSig;
+	NewSig.ModuleName = ModuleName;
+	NewSig.FunctionName = FuncName;
+	NewSig.Signature = Signature;
+	NewSig.Mask = Mask;
+	NewSig.Offset = Offset;
+	NewSig.Address = Address;
+	NewSig.HookFunction = HookAddress;
+	AllSignatures->push_back( NewSig );
 }
 
 void SignatureSearch::Search()
 {
-	printf( "Scanning for signatures.\n" );
-	std::vector<SignatureF>::iterator it;
-	for ( it = allSignatures->begin(); it < allSignatures->end(); it++ )
+	printf( "Scanning for signatures.. \n" );
+
+	// Initialize minhook
+	MH_STATUS stInit = MH_Initialize();
+	if ( stInit == MH_OK )
 	{
-// 		*((void**) it->address) = (void*) (FindPattern( "client.dll", it->funcname, it->signature, it->mask ) + it->offset);
+		printf( "MinHook Initialized! \n" );
+	}
+	else
+	{
+		printf( "MinHook failed to initialize (%i) \n", stInit );
+		return;
+	}
+
+	// Open the process
+	HANDLE hProcess;
+	hProcess = OpenProcess( PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId() );
+	if ( NULL == hProcess )
+	{
+		printf( "Could not open process! \n" );
+		return;
+	}
+
+	// Attempt to hook all registered signatures
+	for( FSignature & Sig : *AllSignatures )
+	{
+		HookSignature( hProcess, Sig );
+	}
+
+	// Done, close the process
+	CloseHandle( hProcess );
+}
+
+void SignatureSearch::HookSignature( HANDLE hProcess, FSignature & Sig )
+{
+	std::cout << "Scanning for: " << Sig.FunctionName << " in " << Sig.ModuleName << std::endl;
+
+	std::wstring wModuleName = std::wstring( Sig.ModuleName.begin(), Sig.ModuleName.end() );
+	HMODULE hModule = SignatureSearch::GetProcessModule( hProcess, wModuleName.c_str() );
+	if ( hModule == NULL )
+	{
+		std::cout << "Could not find module: " << Sig.ModuleName << std::endl;
+		return;
+	}
+	std::cout << "Found module: " << Sig.ModuleName << std::endl;
+
+	MODULEINFO hModuleInfo = { 0 };
+	GetModuleInformation( GetCurrentProcess(), hModule, &hModuleInfo, sizeof( MODULEINFO ) );
+	printf( "\t EntryPoint: 0x%p", hModuleInfo.EntryPoint );
+	printf( "\t lpBaseOfDll: 0x%p", hModuleInfo.lpBaseOfDll );
+	printf( "\t SizeOfImage: 0x%08X", hModuleInfo.SizeOfImage );
+	printf( "\n" );
+	if ( hModuleInfo.EntryPoint == 0 )
+	{
+		printf( "Not sigscanning as entry point was invalid. \n" );
+		return;
+	}
+
+	uintptr_t patternPointer = FindPatternEx( hProcess, hModuleInfo, Sig.Signature.c_str(), Sig.Mask.c_str() );
+	printf( "\t Found patternPointer?: 0x%llx \n", (unsigned long long) patternPointer );
+
+	MH_STATUS stCreateHook = MH_CreateHook( (LPVOID) patternPointer, Sig.HookFunction, reinterpret_cast<LPVOID*>(Sig.Address) );
+	if ( stCreateHook == MH_OK )
+	{
+		printf( "MH_CreateHook success! \n" );
+	}
+	else
+	{
+		printf( "MH_CreateHook failed (%i) \n", stCreateHook );
+		return;
+	}
+
+	MH_STATUS stEnableHook = MH_EnableHook( (LPVOID) patternPointer );
+	if ( stEnableHook == MH_OK )
+	{
+		printf( "MH_EnableHook success! \n" );
+	}
+	else
+	{
+		printf( "MH_EnableHook failed (%i) \n", stEnableHook );
+		return;
 	}
 }
 
-//Internal Pattern scan, external pattern scan is just a wrapper around this
-uintptr_t SignatureSearch::FindPattern( char* base, unsigned int size, char* pattern, char* mask )
+// Internal Pattern scan, external pattern scan is just a wrapper around this
+uintptr_t SignatureSearch::FindPattern( char* base, unsigned int size, const char* pattern, const char* mask )
 {
 	size_t patternLength = strlen( mask );
 
@@ -85,10 +158,10 @@ uintptr_t SignatureSearch::FindPattern( char* base, unsigned int size, char* pat
 	return 0;
 }
 
-//Scan just one module
-uintptr_t SignatureSearch::FindPatternEx( HANDLE hProcess, MODULEINFO hModule, char* pattern, char* mask )
+// Scan just one module
+uintptr_t SignatureSearch::FindPatternEx( HANDLE hProcess, MODULEINFO hModule, const char* pattern, const char* mask )
 {
-	//Grab module information from External Process
+	// Grab module information from External Process
 	uintptr_t start = (uintptr_t) hModule.lpBaseOfDll;
 	uintptr_t end = start + hModule.SizeOfImage;
 
@@ -97,42 +170,42 @@ uintptr_t SignatureSearch::FindPatternEx( HANDLE hProcess, MODULEINFO hModule, c
 
 	while ( currentChunk < end )
 	{
-		//make data accessible to ReadProcessMemory
+		// make data accessible to ReadProcessMemory
 		DWORD oldprotect;
 		VirtualProtectEx( hProcess, (void*) currentChunk, 4096, PROCESS_VM_READ, &oldprotect );
 
-		//Copy chunk of external memory into local storage
+		// Copy chunk of external memory into local storage
 		byte buffer[4096];
 		ReadProcessMemory( hProcess, (void*) currentChunk, &buffer, 4096, &bytesRead );
 
-		//if readprocessmemory failed, return
+		// if readprocessmemory failed, return
 		if ( bytesRead == 0 )
 		{
 			return 0;
 		}
 
-		//Find pattern in local buffer, if pattern is found return address of matching data
+		// Find pattern in local buffer, if pattern is found return address of matching data
 		uintptr_t InternalAddress = FindPattern( (char*) &buffer, bytesRead, pattern, mask );
 
-		//if Find Pattern returned an address
+		// if Find Pattern returned an address
 		if ( InternalAddress != 0 )
 		{
-			//convert internal offset to external address and return
+			// convert internal offset to external address and return
 			uintptr_t offsetFromBuffer = InternalAddress - (uintptr_t) &buffer;
 			return currentChunk + offsetFromBuffer;
 		}
 
-		//pattern not found in this chunk
+		// pattern not found in this chunk
 		else
 		{
-			//advance to next chunk
+			// advance to next chunk
 			currentChunk = currentChunk + bytesRead;
 		}
 	}
 	return 0;
 }
 
-HMODULE SignatureSearch::GetProcessModule( HANDLE hProcess, TCHAR* targetModuleName )
+HMODULE SignatureSearch::GetProcessModule( HANDLE hProcess, const TCHAR* targetModuleName )
 {
 	HMODULE hMods[1024];
 	DWORD cbNeeded;
@@ -150,7 +223,7 @@ HMODULE SignatureSearch::GetProcessModule( HANDLE hProcess, TCHAR* targetModuleN
 				const wchar_t * moduleName = modNameParts[modNameParts.size() - 1].c_str();
 				int cmpName = _tcscmp( moduleName, targetModuleName );
 
-				_tprintf( TEXT( "\t%s (0x%p) [%s == %i]\n" ), szModName, hMods[i], moduleName, cmpName );
+				// _tprintf( TEXT( "\t%s (0x%p) [%s == %i]\n" ), szModName, hMods[i], moduleName, cmpName );
 				if ( cmpName == 0 )
 				{
 					return hMods[i];
@@ -165,20 +238,6 @@ HMODULE SignatureSearch::GetProcessModule( HANDLE hProcess, TCHAR* targetModuleN
 
 void SignatureSearch::DebugInfo()
 {
-	if ( allSignatures )
-	{
-		printf( "Requested signatures: %I64u \n", allSignatures->size() );
-		std::vector<SignatureF>::iterator it;
-		for ( it = allSignatures->begin(); it < allSignatures->end(); it++ )
-		{
-			printf( "%s address: %p \n", it->funcname, it->address );
-		}
-	}
-	else
-	{
-		printf( "No signatures requested. \n" );
-	}
-
 	printf( "Process ID: %u \n", GetCurrentProcessId() );
 	printf( "Modules info: \n" );
 
@@ -205,88 +264,4 @@ void SignatureSearch::DebugInfo()
 	}
 	
 	CloseHandle( hProcess );
-}
-
-// -----------------------------------------------------------------------------
-
-SQRESULT hk_sq_throwerror( HSQUIRRELVM v, const SQChar *err )
-{
-	std::cout << "[ERROR] " << err << std::endl;
-	return fpSqThrowError( v, err );
-}
-
-void SignatureSearch::TestScan()
-{
-	HANDLE hProcess;
-	hProcess = OpenProcess( PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId() );
-	if ( NULL == hProcess )
-	{
-		printf( "Could not open process \n" );
-		return;
-	}
-
-	HMODULE hClient = SignatureSearch::GetProcessModule( hProcess, _T( "client.dll" ) );
-	if ( hClient == NULL )
-	{
-		printf( "Could not find client module \n" );
-		return;
-	}
-
-	printf( "Found client module! \n" );
-
-	MODULEINFO hClientInfo = { 0 };
-	GetModuleInformation( GetCurrentProcess(), hClient, &hClientInfo, sizeof( MODULEINFO ) );
-	printf( "Client module info: \n" );
-	printf( "\t EntryPoint: 0x%p", hClientInfo.EntryPoint );
-	printf( "\t lpBaseOfDll: 0x%p", hClientInfo.lpBaseOfDll );
-	printf( "\t SizeOfImage: 0x%08X", hClientInfo.SizeOfImage );
-	printf( "\n" );
-
-	printf( "Attempt sigscan... \n" );
-	if ( hClientInfo.EntryPoint == 0 )
-	{
-		printf( "Not sigscanning as entry point was invalid. \n" );
-		return;
-	}
-
-	const char* sFuncName = "sq_throwerror";
-	char* sPattern = "\x48\x89\x5C\x24\x08\x48\x89\x74\x24\x10\x57\x48\x83\xEC\x30\x48\x8B\x59\x50";
-	char* sMask = "xxxxxxxxxxxxxxxxxxx";
-
-	uintptr_t patternPointer = FindPatternEx( hProcess, hClientInfo, sPattern, sMask );
-	printf( "\t Found patternPointer?: 0x%llx \n", (unsigned long long) patternPointer );
-
-	MH_STATUS stInit = MH_Initialize();
-	if ( stInit == MH_OK )
-	{
-		printf( "MinHook Initialized! \n" );
-	}
-	else
-	{
-		printf( "MinHook failed to initialize (%i) \n", stInit );
-		return;
-	}
-
-	MH_STATUS stCreateHook = MH_CreateHook( (LPVOID) patternPointer, &hk_sq_throwerror, reinterpret_cast<LPVOID*>(&fpSqThrowError) );
-	if ( stCreateHook == MH_OK )
-	{
-		printf( "MH_CreateHook success! \n" );
-	}
-	else
-	{
-		printf( "MH_CreateHook failed (%i) \n", stCreateHook );
-		return;
-	}
-
-	MH_STATUS stEnableHook = MH_EnableHook( (LPVOID) patternPointer );
-	if ( stEnableHook == MH_OK )
-	{
-		printf( "MH_EnableHook success! \n" );
-	}
-	else
-	{
-		printf( "MH_EnableHook failed (%i) \n", stEnableHook );
-		return;
-	}
-
 }
--- TitanfallSDK/main.cpp
+++ TitanfallSDK/main.cpp
@@ -13,6 +13,18 @@
 #include <fcntl.h>
 #include <vector>
 
+#include <squirrel.h> 
+#include <sqstdio.h> 
+#include <sqstdaux.h> 
+#include <sq/sqvm.h>
+#include <sq/sqstate.h>
+
+#ifdef _MSC_VER
+#pragma comment (lib ,"squirrel.lib")
+#pragma comment (lib ,"sqstdlib.lib")
+#endif
+
+// #include "SquirrelTypes.h"
 #include "Console/Console.h"
 #include "Signatures/SigScan.h"
 
@@ -23,9 +35,55 @@ namespace
 	std::unique_ptr<CConsole> gbl_mConsole;
 }
 
+namespace TitanfallSDK
+{
+
+	struct SQVMSQCollectable
+	{
+		SQUnsignedInteger __vtref; // V-table reference?
+		SQUnsignedInteger _uiRef;
+		struct SQWeakRef *_weakref;
+		SQCollectable *_next;
+		SQCollectable *_prev;
+		SQSharedState *_sharedstate;
+	};
+
+	CREATE_SIGNATURE( "client.dll", SQRESULT, cl_sq_throwerror, hk_cl_sq_throwerror, "\x48\x89\x5C\x24\x08\x48\x89\x74\x24\x10\x57\x48\x83\xEC\x30\x48\x8B\x59\x50", "xxxxxxxxxxxxxxxxxxx", 0, HSQUIRRELVM, const SQChar * );
+// 	CREATE_SIGNATURE( "server.dll", SQRESULT, sv_sq_throwerror, hk_sv_sq_throwerror, "\x48\x89\x5C\x24\x08\x48\x89\x6C\x24\x10\x48\x89\x74\x24\x18\x57\x48\x83\xEC\x20\x48\x8B\x2A", "xxxxxxxxxxxxxxxxxxxxxxx", 0, HSQUIRRELVM, const SQChar * );
+
+	void printfunc( HSQUIRRELVM v, const SQChar *s, ... )
+	{
+		va_list arglist;
+		va_start( arglist, s );
+		vprintf( s, arglist );
+		va_end( arglist );
+	}
+
+	SQRESULT hk_cl_sq_throwerror( HSQUIRRELVM v, const SQChar *err )
+	{
+		std::cout << "[Client] Error on VM: " << v << ": " << err << std::endl;
+
+		const SQVM & vm = *v;
+		SQVMSQCollectable * asdf = (SQVMSQCollectable*) ((void*)v);
+		SQSharedState *_sharedstate = asdf->_sharedstate;
+		printf( " pre print func: %p \n", _sharedstate->_printfunc );
+		_sharedstate->_printfunc = printfunc;
+		printf( "post print func: %p \n", _sharedstate->_printfunc );
+
+		return cl_sq_throwerror( v, err );
+	}
+
+// 	SQRESULT hk_sv_sq_throwerror( HSQUIRRELVM v, const SQChar *err )
+// 	{
+// 		std::cout << "[Server] Error on VM: " << v << ": " << err << std::endl;
+// 		return sv_sq_throwerror( v, err );
+// 	}
+};
+
 extern "C" __declspec(dllexport) DWORD InitializeSDK(LPVOID lpParameter)
 {
 	printf("Icepick injection complete! \n");
+	OpenProcess( PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId() );
 	return 0;
 }
 
@@ -43,10 +101,10 @@ extern "C" __declspec(dllexport) DWORD TestSigscan( LPVOID lpParameter )
 	printf( "Testing c++ sigscan \n" );
 
 // 	SignatureSearch( "sq_throwerror", &sq_throwerror, "\x48\x89\x5C\x24\x08\x48\x89\x74\x24\x10\x57\x48\x83\xEC\x30\x48\x8B\x59\x50", "xxxxxxxxxxxxxxxxxxx", 0 );
-// 	SignatureSearch::Search();
+	SignatureSearch::Search();
 
-	SignatureSearch::DebugInfo();
-	SignatureSearch::TestScan();
+// 	SignatureSearch::DebugInfo();
+// 	SignatureSearch::TestScan();
 
 	return 0;
 }
